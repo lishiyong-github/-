9d3e1acb66cc8ab8fbb86301b509a9933d036122
shiyongli <1525846137@qq.com>
2016-05-19 13:18:53 +0800

    add placeholderTextView and replace it

---
+++ qingdaofu/工具类/PlaceHolderTextView.m
@@ -0,0 +1,21 @@
+//
+//  PlaceHolderTextView.m
+//  qingdaofu
+//
+//  Created by zhixiang on 16/5/19.
+//  Copyright © 2016年 zhixiang. All rights reserved.
+//
+
+#import "PlaceHolderTextView.h"
+
+@implementation PlaceHolderTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
